d06960eadc64b6bf03b6c9bc208278c76f62cbea
marcogomezwong <mg3ta@virginia.edu>
2018-04-05 15:07:14 -0400

    fixed bug

--- Website/testpage.php
+++ Website/testpage.php
@@ -1,5 +1,5 @@
 <?php
-require_one("./library.php");
+require_once("./library.php");
 $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
 if (mysqli_connect_errno()) {
 	echo("Cannot connect to MySQL Server Error code: " .
@@ -9,11 +9,11 @@ if (mysqli_connect_errno()) {
 $sql = "SELECT * FROM Bathrooms;"
 $result = mysqli_query($con, $sql);
 while($row = mysqli_fetch_array($result)) {
-	echo " " . $row['Bathroom_id'];
+	echo $row['Bathroom_id'];
 	echo " " . $row['building_id'];
 	echo " " . $row['floor'];
 	echo " " . $row['longitude'];
-	echo " " . $row['lattitude'];
+	echo " " . $row['latitude'];
 	echo "<br>";
 }
 mysqli_close($con);
