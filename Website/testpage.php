c5c8e1cf953d9c3b3d788e924241d7fab88acf41
marcogomezwong <mg3ta@virginia.edu>
2018-04-05 14:59:26 -0400

    fixed bug

---
+++ Website/testpage.php
@@ -0,0 +1,20 @@
+<?php
+require_one("./library.php");
+$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+if (mysqli_connect_errno()) {
+	echo("Cannot connect to MySQL Server Error code: " .
+	mysqli_connect_error());
+	return null;
+}
+$sql = "SELECT * FROM Bathrooms;"
+$result = mysqli_query($con, $sql);
+while($row = mysqli_fetch_array($result)) {
+	echo " " . $row['Bathroom_id'];
+	echo " " . $row['building_id'];
+	echo " " . $row['floor'];
+	echo " " . $row['longitude'];
+	echo " " . $row['lattitude'];
+	echo "<br>";
+}
+mysqli_close($con);
+?>
