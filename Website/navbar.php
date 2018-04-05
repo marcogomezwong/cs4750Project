168b02cb78a44f5d2fb436845cc927c1d760f6a3
krs2sy <31396254+krs2sy@users.noreply.github.com>
2018-04-04 21:54:42 -0400

    Create navbar.php

---
+++ Website/navbar.php
@@ -0,0 +1,30 @@
+session_start();
+  //Embedding javascript into php based on https://stackoverflow.com/questions/23574306/executing-php-code-inside-a-js-file/23574397
+  $username = "";
+ +
+ +//Checks the logged-in user's username
+  if (isset($_SESSION['username']))
+  {
+      $username = $_SESSION['username'];
+  }
+  header("Content-type: application/javascript");
+ +
+ +//Displays the link for a logged-out user
+  $text1 = "Login";
+  $link1 = "http://localhost:8080/StoryShare_Servlet/LoginServlet";
+ +
+ +//Hides the profile link for a logged-out user
+  $text = "";
+  $link="?";
+  $logout = "";
+ +
+ +//Changes the links for a logged in user
+  if ($username != "") {
+ +
+ +    //Displays the link to the logout servlet for a logged-in user
+      $link1 = "http://localhost:8080/StoryShare_Servlet/LogoutServlet";
+      $text1 = "Logout";
+ +
+ +    //Displays the link to the logged-in user's profile
+      $link = "http://localhost/StoryShare/profile.php";
+      $text = "Welcome, $username";
