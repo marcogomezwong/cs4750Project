90ae1da4bb1e062ff0cc6030a5cc34af509aaf47
Will Cricchi <wcc4ch@virginia.edu>
2018-04-18 16:38:59 -0400

    see reviews is working but needs styling

--- Website/reviewsList.php
+++ Website/reviewsList.php
@@ -68,18 +68,15 @@ mysqli_connect_error());
  // Form the SQL query (a SELECT query)
  //$a = mysql_query("SELECT * FROM `table` WHERE `id` = '1'");
 
+ $id = intval($_GET['bathroom_id']);
 
- $sql="SELECT * FROM Bathrooms WHERE Bathroom_id = 7";
+ $sql="SELECT * FROM Create_review WHERE Bathroom_id = $id";
  $result = mysqli_query($con,$sql);
  // Print the data from the table row by row
  while($row = mysqli_fetch_array($result)) {
- echo "Floor: " . $row['floor'];
+ echo "Rating: " . $row['rating'];
  echo "<br>";
- echo "Overall Rating: " . $row['overall_rating'];
- echo "<br>";
- echo "Longitude: " . $row['longitude'];
- echo "<br>";
- echo "Latitude: " . $row['latitude'];
+ echo "Comment: " . $row['comment'];
  echo "<br>";
  }
  mysqli_close($con);
