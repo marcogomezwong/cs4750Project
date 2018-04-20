13979dd201a564812bb3e1b92e9bac3e490035fd
Marco Gomez <Marco Gomez>
2018-04-20 15:15:41 -0400

    here

--- Website/insertBathroom.php
+++ Website/insertBathroom.php
@@ -7,10 +7,16 @@
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
  }
 
+ $SQL_NAME_TO_ID = "SELECT * Building WHERE  name like 'Rice%;'"
+ if (!mysqli_query($con,$sql))
+ {
+  die('Error: ' . mysqli_error($con));
+ }
+ echo $SQL_NAME_TO_ID;
 
  $sql = "INSERT INTO Bathrooms (overall_rating, rating_count, floor, longitude, latitude)
  VALUES
- ('$_POST[rating]', 1, '$_POST[floor]','$_POST[longitude]','$_POST[latitude]')";
+ ('$_POST[rating]', 1, '$_POST[floor]','$_POST[longitude]','$_POST[latitude]');";
 
  if (!mysqli_query($con,$sql))
  {
