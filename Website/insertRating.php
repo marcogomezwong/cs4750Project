38aecf0f215cd0a4c1780e3c8fdbb1b938744d3f
marcogomezwong <mg3ta@virginia.edu>
2018-04-23 22:19:40 -0400

    fixed?

--- Website/insertRating.php
+++ Website/insertRating.php
@@ -7,19 +7,19 @@
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
  }
 
-$new_rating = $_POST[rating];
+$new_rating = $_POST['rating'];
 $old_rating = 0;
 $count = 0;
 
  $sql = "INSERT INTO Create_review (bathroom_id, rating, comment)
  VALUES
- ('$_POST[bathroom_id]','$_POST[rating]','$_POST[comment]');";
+ ('$_POST['bathroom_id']','$_POST['rating']','$_POST['comment']');";
 
  if (!mysqli_query($con,$sql))
  {
  	die('Error: ' . mysqli_error($con));
 }
- $SQL_select = "SELECT * FROM Bathrooms WHERE Bathroom_id = $_POST[bathroom_id];";
+ $SQL_select = "SELECT * FROM Bathrooms WHERE Bathroom_id = $_POST['bathroom_id'];";
  $result = mysqli_query($con,$SQL_select);
  while($row = mysqli_fetch_array($result)) {
    $old_rating = $row['overall_rating'];
@@ -29,7 +29,7 @@ $count = 0;
 
 $update_val = ($old_rating * $count + (int)$new_rating) / ($count + 1);
 
-$sql_update = "UPDATE Bathrooms SET overall_rating = $update_val WHERE Bathroom_id = $_POST[bathroom_id];";
+$sql_update = "UPDATE Bathrooms SET overall_rating = $update_val WHERE Bathroom_id = $_POST['bathroom_id'];";
 if (!mysqli_query($con,$sql_update))
 {
  die('Error: ' . mysqli_error($con));
