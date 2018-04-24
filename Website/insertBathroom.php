5b5d0e78238b37d548304b6478d001af97fd1d9f
marcogomezwong <mg3ta@virginia.edu>
2018-04-23 20:31:32 -0400

    here

--- Website/insertBathroom.php
+++ Website/insertBathroom.php
@@ -34,10 +34,9 @@ $id = -1;
     while($row_count = mysqli_fetch_array($result_count)) {
         $id = $row_count['building_id'];
     }
-
     echo "id: " + $id + "<br>";
  }
- //
+ 
  // $sql = "INSERT INTO Bathrooms (building_id, overall_rating, rating_count, floor, longitude, latitude)
  // VALUES
  // ($name, '$_POST[rating]', 1, '$_POST[floor]','$_POST[longitude]','$_POST[latitude]');";
